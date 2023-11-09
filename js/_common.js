bef6d141ffeffc82b6a68b77ee8f6d7f72d6ee41
AndressCruz <andress.cruz@fatec.sp.gov.br>
2023-11-09 20:13:35 -0300

    terceiro comentario: adicionado a tela de contatos e responsividade geral

--- js/_common.js
+++ js/_common.js
@@ -4,9 +4,11 @@ function myFunction() {
     if (navbar.className === "nav-transition") {
         navbar.className += " menu";
         document.getElementById("threeline-icon").innerHTML = "&Cross;";
+        document.getElementById("threeline-icon").style.color = "#155263";
     } else {
         navbar.className = "nav-transition";
         document.getElementById("threeline-icon").innerHTML = "&#9776;";
         document.getElementById("threeline-icon").style.textDecoration = "none";
+        document.getElementById("threeline-icon").style.color = "#155263";
     }
 }
